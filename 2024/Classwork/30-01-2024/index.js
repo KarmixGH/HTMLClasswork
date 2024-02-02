490f200fea14c498aa28cf908b6d036b817c9979
modus <modus>
2024-02-02 15:23:41 +0400

    boostrap

--- 2024/Classwork/30-01-2024/index.js
+++ 2024/Classwork/30-01-2024/index.js
@@ -1,4 +1,4 @@
-document.querySelector("h1").style.color="red"
+document.querySelector("h1").style.color = "red";
 
 // let a=15; //number
 // let b="16"; //string
@@ -11,36 +11,163 @@ document.querySelector("h1").style.color="red"
 // array.push('fifth');
 // console.log(array);
 
+
+
+
+
+
 // let fName='Dachi ';
 // let sName='Bitsadze';
 // let fullName=fName+sName;
 // console.log(fullName)
 
+// function adding(a,b){
+//     return a+b;
+// }
+
+// function removing(a,b){
+//     return a-b;
+// }
+
+// function multiplying(a,b){
+//     return a*b;
+// }
+
+// function separation(a,b){
+//     if(b==0){
+//         return 'second parameter is wrong'
+//     }
+
+//     else return a/b;
+// }
+
+// let num1=10;
+// let num2=10;
+
+// let num3=separation(num1, num2);
+// console.log(num3)
 
 
-function adding(a,b){
-    return a+b;
-}
-
-function removing(a,b){
-    return a-b;
-}
-
-function multiplying(a,b){
-    return a*b;
-}
-
-function separation(a,b){
-    if(b==0){
-        return 'second parameter is wrong'
-    }
-
-    else return a/b;
-}
 
 
-let num1=10;
-let num2=10;
 
-let num3=separation(num1, num2);
-console.log(num3)
+
+
+// function comparison(a, b, c) {
+//   if (a > b) {
+//     return a;
+//   }
+//   if (b > c) {
+//     return b;
+//   }
+//   else {
+//     return c;
+//   }
+// }
+
+// function isEven(evenNum) {
+//   return number % 2 == 0;
+// }
+
+// function square(squareNum) {
+//     return squareNum * squareNum;
+// }
+
+// let num1 = 17;
+// let num2 = 14;
+// let num3 = 11;
+// let evenNum = 6;
+// let squareNum = 12;
+
+// let result = comparison(num1, num2, num3);
+// console.log(result);
+
+
+
+
+
+
+
+// let totalMoney = 31;
+// let price = 3;
+
+
+// function purchase(price, totalMoney) {
+//     return Math.floor(totalMoney / price);
+// }
+
+// let purchasedItems = purchase(price, totalMoney);  
+// let remainingMoney = totalMoney % price;
+
+// console.log("Your money:", totalMoney);
+
+// console.log("Price for single unit:", price);
+
+// console.log("Overall quantity of bought items:", purchasedItems); 
+
+// if (price > totalMoney) {
+//     console.log('Get some money first')
+// } if (remainingMoney == 0){
+//     console.log('You have no money left to spend');
+// } else console.log("How much money you have left:", remainingMoney);
+
+
+
+
+
+
+// function createFullText(firsttext, secondtext) {
+//     let fulltext = firsttext + ' ' + secondtext;
+//     return fulltext;
+// }
+
+// let firsttext = 'Bombachka';
+// let secondtext = 'Babaika';
+
+// let fulltext = createFullText(firsttext, secondtext);
+// console.log(fulltext);
+
+
+
+
+
+
+
+// let start = 1000;
+
+// while (start >= 0) {
+//     start -= 7;  
+//     console.log(start);
+// }
+
+
+
+
+// function adding(time){
+//     let totalTime = ++time
+//     return totalTime
+// }
+
+// let counter = 0
+
+// let totalTime = adding(counter)
+// console.log(totalTime)
+
+
+
+// num = 0
+
+// function adding(){
+//     num = ++num
+//     return num
+// }
+
+// console.log(adding())
+// console.log(adding())
+// console.log(adding())
+// console.log(adding())
+// console.log(adding())
+
+
+
+
