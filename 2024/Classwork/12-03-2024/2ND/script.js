1d9082555a69a3ba05e531af9840d34380436e9e
modus <modus>
2024-03-13 18:00:25 +0400

    wow

--- 2024/Classwork/12-03-2024/2ND/script.js
+++ 2024/Classwork/12-03-2024/2ND/script.js
@@ -5,6 +5,8 @@ function idGenerator(){
     return Math.random().toFixed(4).slice(2)
 }
 
+renderUsers(getUsers());
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let formData = new FormData(form);
@@ -13,27 +15,31 @@ form.addEventListener("submit", (event) => {
     saveUser(user);
     renderUsers(getUsers())
     form.reset();
-    console.log(user);
 });
 
 function getUsers(){
-    return JSON.parse(localStorage.getItem("users") ?? [])
+    return JSON.parse(localStorage.getItem("users")) ?? []
 }
 
 function saveUser(user){
     const users=getUsers();
-    let index = users.findIndex(el => el.id === user.id);
+    let index = users.findIndex((el) => { return el.id === user.id });
     index >= 0?users[index]=user: users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
 }
 
-function renderUsers(arr){
-    tbody.innerHTML = "";
-    arr.forEach(user => {
-        tbody.innerHTML += `
-        <tr>
-            <td>
-        `
-    })
+function renderUsers(arr) {
+  tbody.innerHTML = ''; 
+  arr.forEach(user => {
+      tbody.innerHTML += `
+     <tr>
+     <td>${user.id}</td>
+     <td>${user.name}</td>
+     <td>${user.nickname}</td>
+     <td>${user.email}</td>
+     <td>${user.address}</td>
+     <td>${user.phone}</td>
+     <td>${user.website}</td>
+     </tr>`;
+  });
 }
-
